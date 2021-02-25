85e301054c2ab0e0ece3e22ca4a8f5734edeb01b
Laurentiu <dodoloilaurentiu@gmail.com>
2021-02-25 17:40:06 +0200

    Post message to server.

--- homework-1/js/app.js
+++ homework-1/js/app.js
@@ -1,10 +1,20 @@
 var msgInput = document.getElementById('msg-input');
 var sendBtn = document.getElementById('btn-send');
 
-sendBtn.addEventListener('click', function (evt) {
+sendBtn.addEventListener('click', async function (evt) {
     evt.preventDefault();
 
     if (!msgInput.value) {
         return false;
     }
+
+    await axios
+        .post('/message', {
+            content: msgInput.value
+        })
+        .then(r => {
+            console.log(r.data);
+        });
+
+    msgInput.value = '';
 });
